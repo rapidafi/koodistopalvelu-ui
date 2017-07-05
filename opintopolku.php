f8addbf39711370ace1c56c1e5fb0beaea5b96e6
Lauri Jokipii <lauri.jokipii@rapida.fi>
2017-07-05 14:25:36 +0300

    Make library loading work with HTTP or HTTPS both.

--- opintopolku.php
+++ opintopolku.php
@@ -42,7 +42,7 @@ header('Content-Type: text/html; charset=utf-8');
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
 <link rel="stylesheet" href="rapida.css">
